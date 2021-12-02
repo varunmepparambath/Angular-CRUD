7b9d16a1e66bbb065c97245137e82f7bda1ed6ae
Varun Mepparambath <varun.intern@terrain.art>
2021-12-02 15:22:26 +0530

    CRUD Complete

--- src/app/detail/detail.component.ts
+++ src/app/detail/detail.component.ts
@@ -1,6 +1,6 @@
 import { CrudService } from './../service/crud.service';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from './../model/product';
 
 @Component({
@@ -12,21 +12,37 @@ export class DetailComponent implements OnInit {
   public product!: Product;
   public id:any='';
   constructor(private activatedRoute:ActivatedRoute,
-    public crudService : CrudService) { }
+    public crudService : CrudService,
+    private router:Router) { }
+    stringCutter(words:string){
+      var n = words.split(" ");
+      return n[n.length - 1];
+    }
+    delete(id:number){
+      this.crudService.deleteProductById(id).subscribe(res=>{
+        alert('Product Deleted !');
+        this.router.navigateByUrl('home');
+      })
+    }
+
+
+
+
 
   ngOnInit(): void {
     
     this.activatedRoute.paramMap.subscribe((params) => {
 			if (params.get('productId')) {
 				this.id = params.get('productId');
+        this.crudService.getProductById(this.id).subscribe((data: Product) => {
+          console.log(data);
+          this.product = data;
+        })
 			} else {
 				this.id = null;
 			}
 		});
-    this.crudService.getProductById(this.id).subscribe((data: Product) => {
-      console.log(data);
-      this.product = data;
-    })
+    
 
 
   }
--- src/app/update/update.component.ts
+++ src/app/update/update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute, Router, Routes } from '@angular/router';
+import { Product } from '../model/product';
+import { CrudService } from '../service/crud.service';
 
 @Component({
   selector: 'app-update',
@@ -6,10 +10,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
+  public product!: Product;
+  public id:any='';
+ 
+  productForm = new FormGroup({
+    name: new FormControl(''),
+    description: new FormControl(''),
+    price: new FormControl(''),
+    imageUrl: new FormControl(''),
+    quantity: new FormControl(''),
+  });
 
-  constructor() { }
+
+  constructor(public fb: FormBuilder,
+    public crudService:CrudService,
+    private activatedRoute :ActivatedRoute,
+    private router :Router) { }
 
   ngOnInit(): void {
+
+    this.activatedRoute.paramMap.subscribe((params) => {
+			if (params.get('productId')) {
+				this.id = params.get('productId');
+        this.crudService.getProductById(this.id).subscribe((data: Product) => {
+          this.product = data;
+          this.productForm = new FormGroup({
+            name: new FormControl(this.product.name),
+            description: new FormControl(this.product.description),
+            price: new FormControl(this.product.price),
+            imageUrl: new FormControl(this.product.imageUrl),
+            quantity: new FormControl(this.product.quantity),
+          });
+        })
+			} else {
+				this.id = null;
+			}
+		});
+
+  }
+  submitForm() {
+    this.productForm.get('imageUrl')?.setValue('https://loremflickr.com/320/240/');
+    this.crudService.updateProductById(this.product.id, this.productForm.value).subscribe(res => {
+      alert('Product Updated!');
+      this.router.navigateByUrl('home');
+    })
   }
 
 }
